a4496f59ab38007f28dc689522e36990ccaeedf7
Arthur Reis <129776400+friarthur@users.noreply.github.com>
2025-05-30 10:44:56 -0300

    Update .sql

--- back end/.sql
+++ back end/.sql
@@ -4,8 +4,8 @@ USE login_windows;
 CREATE TABLE usuarios (
   id INT AUTO_INCREMENT PRIMARY KEY,
   nome VARCHAR(100) NOT NULL,
-  senha VARCHAR(255) NOT NULL, -- senha criptografada
-  avatar VARCHAR(255) DEFAULT NULL -- opcional (foto do usuário)
+  senha VARCHAR(255) NOT NULL, 
+  avatar VARCHAR(255) DEFAULT NULL 
 );
 INSERT INTO usuarios (nome, senha)
 VALUES ('User', SHA2('1234', 256)); -- senha: 1234
